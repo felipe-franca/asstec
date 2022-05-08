b0eb222551b98ef4e6e89f6a907f9c0f72010832
felipe-franca <felipe.paulino57@gmail.com>
2022-05-08 12:10:34 -0300

    refactor: redirecting user do client portal when reset password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -118,6 +118,56 @@ class ResetPasswordController extends AbstractController
         ]);
     }
 
+    #[Route('/client/reset/{token}', name: 'app_reset_password_client')]
+    public function clientReset(Request $request, UserPasswordHasherInterface $userPasswordHasher, TranslatorInterface $translator, string $token = null): Response
+    {
+        if ($token) {
+            $this->storeTokenInSession($token);
+
+            return $this->redirectToRoute('app_reset_password_client');
+        }
+
+        $token = $this->getTokenFromSession();
+        if (null === $token) {
+            throw $this->createNotFoundException('No reset password token found in the URL or in the session.');
+        }
+
+        try {
+            $user = $this->resetPasswordHelper->validateTokenAndFetchUser($token);
+        } catch (ResetPasswordExceptionInterface $e) {
+            $this->addFlash('error', sprintf(
+                '%s - %s',
+                $translator->trans(ResetPasswordExceptionInterface::MESSAGE_PROBLEM_VALIDATE, [], 'ResetPasswordBundle'),
+                $translator->trans($e->getReason(), [], 'ResetPasswordBundle')
+            ));
+
+            return $this->redirectToRoute('app_forgot_password_request');
+        }
+
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->resetPasswordHelper->removeResetRequest($token);
+
+            $encodedPassword = $userPasswordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+
+            $user->setPassword($encodedPassword);
+            $this->entityManager->flush();
+
+            $this->cleanSessionAfterReset();
+
+            return $this->redirectToRoute('app_client_home');
+        }
+
+        return $this->render('reset_password/reset.html.twig', [
+            'resetForm' => $form->createView(),
+        ]);
+    }
+
     private function processSendingPasswordResetEmail(string $emailFormData, MailerInterface $mailer, TranslatorInterface $translator): RedirectResponse
     {
         $user = $this->entityManager->getRepository(User::class)->findOneBy([
@@ -144,7 +194,10 @@ class ResetPasswordController extends AbstractController
             ->from(new Address('helptechfmu@gmail.com', 'Equipe HelpTech'))
             ->to($user->getEmail())
             ->subject('Solicitação de alteração de senha')
-            ->html($this->renderView('reset_password/email.html.twig', ['resetToken' => $resetToken]));
+            ->html($this->renderView($user->getOccupation(User::CLIENT_OCCUPATION)
+                ? 'reset_password/client_email.html.twig'
+                :'reset_password/email.html.twig',
+                ['resetToken' => $resetToken]));
 
         $transport = new GmailSmtpTransport('helptechfmu@gmail.com','HelpTechFMU2022!');
         $mailer = new Mailer($transport);
